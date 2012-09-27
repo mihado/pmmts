7612def7d6b5661a2dd08a8634ef4c37e1fa9e6e
Minh Ha <nhat.minh.ha@gmail.com>
2012-09-28 03:41:57 +1200

    initialised

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pmmts::Application.config.secret_token = 'd111c60a1486c5c56e930436d41aebf417ce94686f71aa668cbec00a70f68b8f36628cfe6f4f76305f2c456ab4c5dc737b976834093e16ca7b13259b816145c3'
