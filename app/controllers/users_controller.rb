d1f6b6da0fd6eab8e0e3ec39bd6ba2d41c4a18b7
Minh Ha <nhat.minh.ha@gmail.com>
2012-09-28 04:45:29 +1200

    edit users_controller.rb for creating new user, still need to write code for session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			session[:user_id] = @user.user_id
+			redirect_to root_url, notice: "Signed up"
+		else
+			render "new"
+		end
+	end
 end
