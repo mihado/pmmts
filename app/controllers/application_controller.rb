94c017fbde7c1848312cfa4a51eb769879f10c9f
Minh Ha <nhat.minh.ha@gmail.com>
2012-09-28 06:35:05 +1200

    enabling Log Out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
+
+	private
+# Some private methods
+	# use this method to fetch the current_user in order for the username to be display when logged in
+	# fetch current_user based on session[:id] which is defined in sessions_controller (if the session variable exists)
+	# @current_user | cached the session variable => fetch once / request
+	def current_user
+		@current_user ||= User.find(session[:id]) if session[:id]
+	end
+
+	# make this method accessible from inside the views
+	helper_method :current_user
+
 end
