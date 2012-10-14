6d98ecef46623207d954f65f183b584f1acaed56
Minh Ha <nhat.minh.ha@gmail.com>
2012-10-15 11:27:52 +1300

    tweak some css n' layouts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,8 @@ class SessionsController < ApplicationController
 		user = User.find_by_username(params[:username])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-			redirect_to root_url, notice: "Logged in!"
+			#redirect_to root_url, notice: "Logged in!"
+			redirect_to root_url
 		else
 			flash.now.alert = "Username or password is invalid"
 			render "new"
@@ -24,7 +25,8 @@ class SessionsController < ApplicationController
 	def destroy
 		session[:user_id] = nil
 		reset_session
-		redirect_to root_url, notice: "Logged out!"
+		#redirect_to root_url, notice: "Logged out!"
+		redirect_to root_url
 	end
 
 end
