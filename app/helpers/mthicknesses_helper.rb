2f1eb3d2aee4aa4d7bf29720bab4ceb265d635d2
Minh Ha <nhat.minh.ha@gmail.com>
2012-10-17 09:26:02 +1300

    finished select_tags for search

--- app/helpers/mthicknesses_helper.rb
+++ app/helpers/mthicknesses_helper.rb
@@ -1,7 +1,6 @@
 module MthicknessesHelper
 #TODO: turn this one into a proper select_tag
 	def mthickness_array_for_select
-		mthickness_array = Mthickness.all.map { |mthickness| [mthickness.id, mthickness.thickness]  }
-		options_for_select(mthickness_array)
+		mthickness_array = Mthickness.all.map { |mthickness| [mthickness.thickness, mthickness.thickness]  }
 	end
 end
--- app/helpers/mtypes_helper.rb
+++ app/helpers/mtypes_helper.rb
@@ -1,7 +1,6 @@
 module MtypesHelper
 #TODO: turn this one into a proper select_tag
 	def mtype_array_for_select
-		mtype_array = Mtype.all.map { |mtype| [mtype.id, mtype.code]  }
-		options_for_select(mtype_array)
+		mtype_array = Mtype.all.map { |mtype| [mtype.code, mtype.code]  }
 	end
 end
